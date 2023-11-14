eda9a0708588e0f904a121342b2be5db6a034a57
Marc Hirschvogel <marc.hirschvogel@sounddive.de>
2023-11-14 12:23:05 +0100

    Revamp docs structure

---
+++ docs/source/conf.py
@@ -0,0 +1,28 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'ambit'
+copyright = '2023, Marc Hirschvogel'
+author = 'Marc Hirschvogel'
+release = '1.1.3'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ["myst_parser"] # to have Markdown working with Sphinx...
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
---
+++ docs/source/index.rst
@@ -0,0 +1,2 @@
+Welcome to Ambit's documentation!
+=================================
