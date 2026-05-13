Fix time computation when restarting with different dt

demos/fluid/fluid_multiphase_rising_bubble.py
@@ -13,7 +13,7 @@ from pathlib import Path
 def main():
     basepath = str(Path(__file__).parent.absolute())
 
-    # cases (1,2) from Eikelder et al. (2024), Brunk and Eikelder (2026)
+    # cases (1,2) from ten Eikelder et al. (2024), Brunk and ten Eikelder (2026)
     case = 1
 
     IO_PARAMS = {
@@ -41,7 +41,7 @@ def main():
     IO_PARAMS["simname"] += "_elx"+str(IO_PARAMS["mesh_domain"]["meshsize"][0])
 
     h = 1.0/IO_PARAMS["mesh_domain"]["meshsize"][0] # element edge length
-    eps = 0.64*h # 1.28*h (Eikelder et al. (2024)), 0.64*h (Brunk and Eikelder (2026))
+    eps = 0.64*h # 1.28*h (ten Eikelder et al. (2024)), 0.64*h (Brunk and ten Eikelder (2026))
 
     """
     The initial phase field (phi=-1 is surrounding fluid 1, phi=1 is bubble)
@@ -64,7 +64,7 @@ def main():
     Parameters for the global time control
     """
     CONTROL_PARAMS = {"maxtime": 3.0,
-                      "dt": 0.128*h, # from Eikelder et al. (2024)
+                      "dt": 0.128*h, # from ten Eikelder et al. (2024)
                       "initial_fields": [expr1, None],
                       }
 
@@ -140,14 +140,14 @@ def main():
     elif case==2:
         rho1 = 1000.0
         rho2 = 1.0
-        eta1 = 10.0   # 10.0, 1.0 TODO: Ambiguity code (10.0) vs. papers (1.0)
+        eta1 = 10.0   # NOTE: Ambiguity code (10.0) vs. papers (1.0) - Author confirmation by M. ten Eikelder: 10.0 is the correct value!
         eta2 = 0.1
         sig = 1.96 # surface energy density coefficient
     else:
         raise ValueError("Unknown case.")
     zeta = 0.0
 
-    alpha = (rho1-rho2)/(rho1+rho2) # TODO: Negative in Eikelder et al. (2024), Brunk and Eikelder (2026), but then not working!
+    alpha = (rho1-rho2)/(rho1+rho2) # TODO: Negative in ten Eikelder et al. (2024), Brunk and ten Eikelder (2026), but then not working!
     sigtilde = 3.*sig/(2.*np.sqrt(2.))
 
     class locate_all:

src/ambit_fe/base.py
@@ -128,6 +128,9 @@ class solver_base:
             self.pb.numdof,
         )
 
+        # initial time
+        self.t0 = 0.0
+
         # model order reduction stuff
         if self.pb.pbase.have_rom:
             from .mor import mor_main
@@ -140,8 +143,9 @@ class solver_base:
 
         # read restart information if requested - TODO: Maybe move to evaluate_system_initial, but needs thorough checking!
         self.pb.read_restart(self.pb.pbase.simname, self.pb.pbase.restart_step)
-        # update simname
+        # read time and update simname
         if self.pb.pbase.restart_step > 0:
+            self.read_step_time(self.pb.pbase.restart_step)
             self.pb.pbase.simname += "_r" + str(self.pb.pbase.restart_step)
 
         self.initialize_nonlinear_solver()
@@ -201,7 +205,7 @@ class solver_base:
             wts = time.time()
 
             # current time
-            t = N * self.pb.pbase.dt
+            t = self.t0 + (N-self.pb.pbase.restart_step) * self.pb.pbase.dt
 
             # evaluate any (solution-independent) time curves or other functions
             self.pb.evaluate_pre_solve(t, N, self.pb.pbase.dt)
@@ -235,6 +239,7 @@ class solver_base:
 
             # write restart information if desired
             self.pb.write_restart(self.pb.pbase.simname, N)
+            self.write_step_time(N, t)
 
             # update nonlinear and linear iteration counters
             self.update_counters(wt, t)
@@ -368,6 +373,20 @@ class solver_base:
                 )
                 f.write("%.16E %.16E %i %i\n" % (t, wt, self.solnln.ni, self.solnln.li))
                 f.close()
+
+    def write_step_time(self, N, t):
+        if (self.pb.io.write_restart_every > 0 and N % self.pb.io.write_restart_every == 0):
+            if self.pb.comm.rank == 0:
+                f = open(
+                    self.pb.pbase.output_path + "/checkpoint_" + self.pb.pbase.simname + "_step_time_" + str(N) + ".txt",
+                    "wt",
+                )
+                f.write("%i %.16E\n" % (N, t))
+                f.close()
+
+    def read_step_time(self, rstep):
+        restart_data_time = np.loadtxt(self.pb.pbase.output_path + "/checkpoint_" + self.pb.pbase.simname + "_step_time_" + str(rstep) + ".txt", ndmin=1)
+        self.t0 = restart_data_time[1]
 
     def reset_counters(self):
         self.wt, self.ni, self.li = 0.0, 0, 0